63fb66d4802b001aa4f5e0cdebc15297f035b612
sjoerdvgerwen <63058506+sjoerdvgerwen@users.noreply.github.com>
2021-06-11 17:15:15 +0200

    Test brench

--- Brenches/Program.cs
+++ Brenches/Program.cs
@@ -6,7 +6,7 @@ namespace Brenches
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           Console.WriteLine("Jemoeder");
         }
     }
 }
